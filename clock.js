422d4ea6238805d7a30a95e72bcf09228d75db7e
walidrudro <walid.rudro@outlook.com>
2020-06-08 14:02:28 +1200

    first commit

---
+++ clock.js
@@ -0,0 +1,11 @@
+setInverval( function () {
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var seconds = currentTime.getSeconds();
+
+
+
+
+	
+})
